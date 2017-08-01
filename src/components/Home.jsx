28fa280fc05013d5e52b00c2a6b22f3b53824403
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-08-01 15:27:50 +0100

    Dynamic config settings ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Section from 'grommet/components/Section';
 import Anchor from 'grommet/components/Anchor';
 import { Actions } from 'jumpstate';
 import { Link } from 'react-router-dom';
-import config from '../config';
+import { config } from '../config';
 
 // TODO: Look into grommet's path prop for buttons to see if it works with React-Router v4
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
     <Section colorIndex="light-2">
-      <Anchor path="/astro" onClick={handleSettingProjectCollection.bind(null, config.dev.astroProjects)}>Introduction to Astronomy</Anchor>
+      <Anchor path="/astro" onClick={handleSettingProjectCollection.bind(null, config.astroProjects)}>Introduction to Astronomy</Anchor>
       <Anchor path="/" onClick={handleSettingProjectCollection.bind(null, [])}>Wildcam Darian: Labs</Anchor>
     </Section>
   );
--- src/config.js
+++ src/config.js
@@ -1,12 +1,46 @@
-const config = {
-  dev: {
-    panoptesAppId: '397e9bf4e29e75c0a092261ebe3338d3ef2687f2c5935d55c7ca0f63ecc2dd33', // test-rog project for dev
+/*
+Configuration Settings
+----------------------
+
+The config settings change depending on which environment the app is running in.
+By default, this is the development environment, but this can be changed either by:
+
+* An env query string, e.g. localhost:3998?env=production
+  (This changes the Panoptes JS Client does)
+* The NODE_ENV environment variable on the system running the app.
+
+ */
+
+var DEFAULT_ENV = 'development';
+var envFromBrowser = locationMatch(/\W?env=(\w+)/);
+var envFromShell = process.env.NODE_ENV;
+var env = envFromBrowser || envFromShell || DEFAULT_ENV;
+
+if (!env.match(/^(production|staging|development)$/)) {
+  throw new Error(`Error: Invalid Environment - ${envFromShell}`);
+}
+
+const baseConfig = {
+  'development': {
+    panoptesAppId: '397e9bf4e29e75c0a092261ebe3338d3ef2687f2c5935d55c7ca0f63ecc2dd33',
     astroProjects: ['1315']
   },
-  production: {
+  'production': {
     panoptesAppId: '',
     astroProjects: []
-  }
+  },
 };
+baseConfig.staging = baseConfig.development;
 
-export default config;
+const config = baseConfig[env];
+export { env, config };
+
+// Try and match the location.search property against a regex. Basically mimics
+// the CoffeeScript existential operator, in case we're not in a browser.
+function locationMatch(regex) {
+  var match;
+  if (typeof location !== 'undefined' && location !== null) {
+    match = location.search.match(regex);
+  }
+  return (match && match[1]) ? match[1] : undefined;
+}
