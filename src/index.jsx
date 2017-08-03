b4eb411e5b485a122a01d7e4722ae0122ff8a1b7
srallen <srallen@users.noreply.github.com>
2017-08-03 09:01:18 -0500

    Use hash history router instead of browser history (#16)

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import oauth from 'panoptes-client/lib/oauth';
 import apiClient from 'panoptes-client/lib/api-client';
@@ -13,14 +12,13 @@ import configureStore from './store';
 import './styles/main.styl';
 
 const store = configureStore();
-const history = createHistory();
 
 // TODO: Make app ID dynamic between env
 oauth.init(config.panoptesAppId)
   .then(() => {
     ReactDOM.render((
       <Provider store={store}>
-        <Router history={history}>
+        <Router>
           <Route path="/" component={Main} />
         </Router>
       </Provider>),
