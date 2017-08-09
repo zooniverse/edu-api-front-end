1f0a6139336ba9a5e31951d381e21a3771d2016a
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-08-09 14:35:17 +0100

    Updated ClassroomManagerContainer

--- src/containers/common/ClassroomManagerContainer.jsx
+++ src/containers/common/ClassroomManagerContainer.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Actions } from 'jumpstate';
+import {
+  CLASSROOMS_STATUS, CLASSROOMS_INITIAL_STATE, CLASSROOMS_PROPTYPES,
+} from '../../ducks/classrooms';
+  
+import Section from 'grommet/components/Section';
+import Heading from 'grommet/components/Heading';
+import List from 'grommet/components/List';
+import ListItem from 'grommet/components/ListItem';
 
 class ClassroomManagerContainer extends React.Component {
   constructor(props) {
@@ -13,27 +21,51 @@ class ClassroomManagerContainer extends React.Component {
   }
 
   render() {
-    if (this.props.fetching) {
-      return (<div>Loading...</div>);
-    }
+    return (
+      <Section>
+        <Heading tag="h3">Classrooms</Heading>
+        {this.render_status()}
+        <List>
+          {this.props.status === CLASSROOMS_STATUS.SUCCESS &&
+            this.props.classrooms.map((classroom, i)=>{
+            return (
+              <ListItem key={`classroom_${i}`}>
+                <div>{classroom.id}</div>
+                <div>{classroom.name}</div>
+                <div>{classroom.description}</div>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Section>
+    );
+  }
 
-    return (<div>{this.props.classrooms}</div>);
+  render_status() {
+    if (this.props.status === CLASSROOMS_STATUS.FETCHING) {
+      return (<div>Loading...</div>);
+    } else if (this.props.status === CLASSROOMS_STATUS.ERROR) {
+      return (<div>ERROR!</div>);
+    } else if (this.props.status === CLASSROOMS_STATUS.SUCCESS) {
+      return (<div>Ready!</div>);
+    }
+      
+    return null;
   }
 }
 
 ClassroomManagerContainer.propTypes = {
-  classrooms: PropTypes.arrayOf(PropTypes.object),
-  fetching: PropTypes.bool
+  ...CLASSROOMS_PROPTYPES,
 };
 
 ClassroomManagerContainer.defaultProps = {
-  classrooms: [],
-  fetching: false
+  ...CLASSROOMS_INITIAL_STATE,
 };
 
 const mapStateToProps = (state) => ({
   classrooms: state.classrooms.classrooms,
-  fetching: state.classrooms.fetching
+  error: state.classrooms.error,
+  status: state.classrooms.status,
 });
 
 export default connect(mapStateToProps)(ClassroomManagerContainer);
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import oauth from 'panoptes-client/lib/oauth';
 import apiClient from 'panoptes-client/lib/api-client';
