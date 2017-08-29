b00b4948ff84060c3aa75f5710fb5f94117a49f0
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2017-08-29 19:18:20 +0100

    Fix Edu API URL for staging (#42)

--- src/lib/config.js
+++ src/lib/config.js
@@ -24,7 +24,7 @@ const baseConfig = {
   'development': {
     astroProjects: ['1315'],
     panoptesAppId: '397e9bf4e29e75c0a092261ebe3338d3ef2687f2c5935d55c7ca0f63ecc2dd33',
-    root: 'https://education.staging.zooniverse.org/'
+    root: 'https://education-api-staging.zooniverse.org/'
   },
   'production': {
     astroProjects: [],
